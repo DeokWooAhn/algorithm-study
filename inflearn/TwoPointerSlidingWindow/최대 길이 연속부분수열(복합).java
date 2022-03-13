60815403528d75eaef1b038fd5b4450a67d6cb52
DoekWooAhn <fjrlvkdlxj@naver.com>
2022-03-14 00:11:36 +0900

    2022.03.14

--- inflearn/TwoPointerSlidingWindow/최대 길이 연속부분수열(복합).java
+++ inflearn/TwoPointerSlidingWindow/최대 길이 연속부분수열(복합).java
@@ -7,17 +7,38 @@ public class Main {
 	public int soloution(int n, int k, int[] arr) {
 		
 		int answer = 0;
+		int lang = 0;
 		int cnt = 0;
 		int p2 = 0;
-				
-		for(int p1=0; p1<n; p1++) {
-			if(arr[p1] == 0) cnt++;
-			while(cnt>k) {
-				if(arr[p2] == 0) cnt--;
-				p2++;
+		
+		for(int p1 = 0; p1<n; p1++) {
+			if(arr[p1] == 0) {
+				cnt++;
 			}
-			answer = Math.max(answer, p1-p2+1);
+			
+			lang++;
+			
+			while(cnt>k) {
+				if(arr[p2] == 0) {
+					cnt--;
+					
+				}
+				p2++;
+				lang--;
+				if(lang>answer) {
+					answer = lang;
+				}
+			}
 		}
+		
+//		for(int p1=0; p1<n; p1++) {
+//			if(arr[p1] == 0) cnt++;
+//			while(cnt>k) {
+//				if(arr[p2] == 0) cnt--;
+//				p2++;
+//			}
+//			answer = Math.max(answer, p1-p2+1);
+//		}
 
 		return answer;
 	}
