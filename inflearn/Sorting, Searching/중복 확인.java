df5b3d9718dde385943718e881d8d5cd53b6b76d
DoekWooAhn <fjrlvkdlxj@naver.com>
2022-04-02 20:38:53 +0900

    2022.04.02

---
+++ inflearn/Sorting, Searching/중복 확인.java
@@ -0,0 +1,31 @@
+package inflearn;
+
+import java.util.*;
+
+public class Main {
+    
+    public String solution(int n, int[] arr) {
+    	String answer = "U";
+    	Arrays.sort(arr);
+    	
+    	for(int i=0; i<n-1; i++) {
+    		if(arr[i] == arr[i+1]) {
+    			return "D";
+    		}
+    	}
+    	return answer;
+    }
+    
+    public static void main(String args[]) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, arr));
+    }
+}
