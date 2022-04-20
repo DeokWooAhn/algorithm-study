f8b56701ed6b8214f27fd606ced408d1b83a0e89
DoekWooAhn <fjrlvkdlxj@naver.com>
2022-04-21 01:29:16 +0900

    2022.04.21

---
+++ inflearn/DFS, BFS 활용/동전교환.java
@@ -0,0 +1,38 @@
+package inflearn;
+
+import java.util.*;
+
+public class Main {
+	static int answer = 600;
+	static int n, m;
+	
+	public void DFS(int L, int sum, Integer[] arr) {
+		if(sum>m) return;
+		if(L>=answer) return;
+		
+		if(sum == m) {
+			if(answer>L) answer = L;
+		}
+		
+		else {
+			for(int i=0; i<n; i++) {
+				DFS(L+1, sum+arr[i], arr);
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt();
+		
+		Integer[] arr = new Integer[n];
+		for(int i=0; i<n; i++) arr[i] = kb.nextInt();
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		m = kb.nextInt();
+		
+		T.DFS(0, 0, arr);
+		System.out.println(answer);
+	}
+}
