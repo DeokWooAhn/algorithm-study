edbb94a3f243c7ba552efcba456879eee0f66d47
DoekWooAhn <fjrlvkdlxj@naver.com>
2022-04-29 00:55:56 +0900

    2022.04.29

---
+++ inflearn/DFS, BFS 활용/피자 배달 거리.java
@@ -0,0 +1,65 @@
+package inflearn;
+
+import java.util.*;
+
+class Point {
+	int x, y;
+	Point(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+}
+
+public class Main {
+	static int answer = 0;
+	static int n;
+	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+	
+	public void BFS(int x, int y, int[][] board) {
+		Queue <Point> queue = new LinkedList<>();
+		queue.add(new Point(x,y));
+		while(!queue.isEmpty()) {
+			Point pos = queue.poll();
+			for(int i=0; i<8; i++) {
+				int nx = pos.x+dx[i];
+				int ny = pos.y+dy[i];
+				
+				if(nx>=0 && nx<n && ny>=0 && ny<n && board[nx][ny]==1) {
+					board[nx][ny] = 0;
+					queue.add(new Point(nx, ny));
+				}
+			}
+		}
+		
+	}
+	
+	public void solution(int[][] board) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(board[i][j]==1) {
+					answer++;
+					board[i][j]=0;
+					BFS(i, j, board);
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		
+		n = kb.nextInt();
+		
+		int[][] arr = new int[n][n];
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				arr[i][j] = kb.nextInt();
+			}
+		}
+		T.solution(arr);
+		System.out.println(answer);
+	}
+}
