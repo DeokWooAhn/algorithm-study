3023274a31117a1f06e270984976a6e65f9ca236
안덕우 <fjrlvkdlxj@naver.com>
2022-05-10 23:24:11 +0900

    2022.05.10

--- inflearn/Array/가위 바위 보.java
+++ inflearn/Array/가위 바위 보.java
@@ -1,54 +1,38 @@
-package inflearn;
-
 import java.util.*;
 
-public class Main {
-	
-	public String solution(int n, int[] a, int[] b) {
-		
-		String answer = "";
-		
-		for(int i = 0; i<n; i++) {
-			switch(a[i]) {
-			case 1:
-				if(b[i] == 1) answer += "D";
-				else if(b[i] == 2) answer += "B";
-				else answer += "A";
-				break;
-			case 2:
-				if(b[i] == 1) answer += "A";
-				else if(b[i] == 2) answer += "D";
-				else answer += "B";
-				break;
-			case 3:
-				if(b[i] == 1) answer += "B";
-				else if(b[i] == 2) answer += "A";
-				else answer += "D";
-				break;
+public class Solution {
+	public int[] solution(int n, String[] arr) {
+		int[] answer = new int[n];
+		int cnt = 0;
+
+		for(int i=0; i<arr.length; i++) {
+
+			for(int j=0; j<arr[i].length(); j++) {
+				if(arr[i].charAt(j) == 'O') {
+					cnt++;
+				}
+				else cnt = 0;
+				answer[i] += cnt;
 			}
+
 		}
-		
+
 		return answer;
 	}
-	
+
 	public static void main(String args[]) {
-		Main T = new Main();
+		Solution T = new Solution();
 		Scanner kb = new Scanner(System.in);
-		
+
 		int n = kb.nextInt();
-		int[] a = new int[n];
-		int[] b = new int[n];
-		
-		for(int i = 0; i<n; i++) {
-			a[i] = kb.nextInt();
+		String[] arr = new String[n];
+
+		for(int i=0; i<n; i++) {
+			arr[i] = kb.next();
 		}
-		
-		for(int i = 0; i<n; i++) {
-			b[i] = kb.nextInt();
-		}
-		
-		for(char x : T.solution(n, a, b).toCharArray()) {
-			System.out.println(x);
+
+		for(int i : T.solution(n, arr)) {
+			System.out.println(i);
 		}
 	}
 }
