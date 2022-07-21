7708d9aa19ba9e0b4970720087a17628ec7d3198
안덕우 <fjrlvkdlxj@naver.com>
2022-07-21 23:38:20 +0900

    2022.07.21

---
+++ programmers/Level2/피보나치 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int[] answer = new int[n+1];
+
+        for(int i=0; i<=n; i++) {
+            if(i == 0) answer[i] = 0;
+            else if(i == 1) answer[i] = 1;
+            else answer[i] = (answer[i-2] + answer[i-1]) % 1234567;
+        }
+
+        return answer[n];
+    }
+
+    public static void main(String args[]) {
+        Solution T = new Solution();
+
+        int n = 3;
+
+        System.out.println(T.solution(n));
+    }
+}
